abb9146b36f00b473578895b01af2b5f429b7f53
Dima Semenov <semenovdima998@gmail.com>
2021-01-12 18:06:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.b41ed4a4735cf8d0f13d307dd5812f26.js"
 );
 
 self.addEventListener('message', (event) => {
